d368fd22ebf1fb2f70211c20fbc2a507a904ec80
testrunner100 <mgrayson15.6155000@outlook.com>
2025-09-17 16:45:05 +0100

    034_summarising - completed TASK

--- 034_summarising.py
+++ 034_summarising.py
@@ -40,7 +40,7 @@ print(text)
 
 another_text = "\n".join(lines)
 # Uncomment this next line if you want to see it:
-# print(another_text)
+print(another_text)
 
 # `join` is actually little smarter — it only adds the `\n` character between
 # lines, not at the end also.
@@ -52,6 +52,13 @@ print("Function: add_up_numbers")
 
 # Add up all the numbers in the list
 def add_up_numbers(numbers):
+  sum = 0
+  
+  for num in numbers:
+    sum = sum + num
+
+  return sum
+
   pass
 
 check_that_these_are_equal(
