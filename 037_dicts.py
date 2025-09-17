0f33ffec097a90432caaedcdcea2865e0a8e218f
testrunner100 <mgrayson15.6155000@outlook.com>
2025-09-17 17:16:30 +0100

    037_dicts - completed TASK

--- 037_dicts.py
+++ 037_dicts.py
@@ -17,6 +17,7 @@
 my_dictionary = {
   "String": "A sequence of characters",
   "List": "A sequence of any item",
+  "Dictionary": "A collection of keys mapped to values"
 }
 
 # Note that:
@@ -37,6 +38,6 @@ print("  " + my_dictionary["List"])
 # the code around line 20. Then print out the value below.
 
 print("A Dictionary is:")
-# ...
+print("  " + my_dictionary["Dictionary"])
 
 # Once you're done, move on to 038_dict_operations.py
